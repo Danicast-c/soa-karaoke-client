9a5f744924903738a08587f610b5a94f94988714
Gustavo Segura <yo@gustavosegura.com>
2021-10-09 16:24:27 -0600

    edit song

--- pages/edit_song.vue
+++ pages/edit_song.vue
@@ -4,14 +4,19 @@
   </p>
   <p v-else-if="$fetchState.error">Error while fetching song 🤬</p>
   <div v-else>
+
+    <audio src="https://storage.googleapis.com/soa-karaoke-canciones/09b2f3a7-7602-4f96-0846-f74292deb147"></audio>
     <form @submit.prevent="submit">
-      <h1>New song</h1>
+      <h1>Edit song</h1>
       <v-text-field v-model="song.title" label="Song Name"></v-text-field>
       <v-text-field v-model="song.artist" label="Artist Name"></v-text-field>
       <v-text-field v-model="song.album" label="Song Name"></v-text-field>
       <LyricsLines :lines="this.song.lyrics" />
       <v-btn class="mr-4" type="submit"> submit </v-btn>
     </form>
+
+
+
   </div>
 
   <!-- <v-container :loading="loadingVar">
