04560e31c47a769a7a834c1422f12400c75818e2
Gustavo Segura <yo@gustavosegura.com>
2021-10-10 01:14:36 -0600

    hide edit and create new song to basic users

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,16 +2,17 @@
   <v-app>
     <v-app-bar app color="green">
       <v-btn text to="/"><h1>Karaoke 99</h1></v-btn>
-      <v-spacer/>
+      <v-spacer />
       <div v-if="$auth.loggedIn">
-        <v-btn text @click="logout()">Logout</v-btn> 
-        <v-btn text to="/new_song">New Song</v-btn>
+        <v-btn text @click="logout()">Logout</v-btn>
+        <div v-if="$auth.user.role=='premium'">
+          <v-btn text to="/new_song">New Song</v-btn>
+        </div>
       </div>
       <div v-else>
         <v-btn text to="/login">Login</v-btn>
         <v-btn text to="/register">Register</v-btn>
       </div>
-
     </v-app-bar>
     <v-main>
       <nuxt />
@@ -20,14 +21,13 @@
 </template>
 
 <script>
-
 export default {
   methods: {
-    logout(){
-      this.$auth.logout()
-      alert('logged in? '+this.$auth.loggedIn)
-      this.$router.push('login');
-    }
-  }
-}
+    logout() {
+      this.$auth.logout();
+      alert("logged in? " + this.$auth.loggedIn);
+      this.$router.push("login");
+    },
+  },
+};
 </script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-<v-card>
+  <v-card>
     <!-- <v-row justify="center" align="center">
     <v-col cols="12" sm="8" md="6">
       <v-card class="logo py-4 d-flex justify-center">
@@ -7,90 +7,152 @@
       </v-card> -->
     <!-- tabla -->
     <v-card-title>
-        Song
-        <v-spacer></v-spacer>
-        <v-text-field v-model="search" append-icon="mdi-magnify" label="Search" single-line hide-details></v-text-field>
+      Song
+      <v-spacer></v-spacer>
+      <v-text-field
+        v-model="search"
+        append-icon="mdi-magnify"
+        label="Search"
+        single-line
+        hide-details
+      ></v-text-field>
     </v-card-title>
 
-    <v-data-table :headers="headers" :items="songs" :items-per-page="100"  :search="search" class="elevation-1" :loading="loadingVar" loading-text="Loading Songs...">
-        <!-- @click:row="songClick" -->
+    <v-data-table
+      :headers="filteredHeaders"
+      :items="songs"
+      :items-per-page="100"
+      :search="search"
+      class="elevation-1"
+      :loading="loadingVar"
+      loading-text="Loading Songs..."
+    >
+      <!-- @click:row="songClick" -->
 
-        <template v-slot:item.play="{ item }">
-            <v-btn-toggle>
-                <v-btn variant="primary" @click="playSong(item)">
-                    <v-icon>mdi-play</v-icon>
-                </v-btn>
-            </v-btn-toggle>
-        </template>
+      <template v-slot:item.play="{ item }">
+        <v-btn-toggle>
+          <v-btn variant="primary" @click="playSong(item)">
+            <v-icon>mdi-play</v-icon>
+          </v-btn>
+        </v-btn-toggle>
+      </template>
 
-        <template v-slot:item.actions="{ item }">
-            <v-btn-toggle>
-                <v-btn variant="primary" @click="editSong(item)">
-                    <v-icon>mdi-pencil</v-icon>
-                </v-btn>
-            </v-btn-toggle>
-        </template>
+      <template v-slot:item.actions="{ item }">
+        <v-btn-toggle>
+          <v-btn variant="primary" @click="editSong(item)">
+            <v-icon>mdi-pencil</v-icon>
+          </v-btn>
+        </v-btn-toggle>
+      </template>
     </v-data-table>
+
     <!-- </v-col> -->
     <!-- </v-row> -->
-</v-card>
+
+    <!-- <div class="text-center">
+      <v-dialog v-model="buy_premium_dialog" width="500">
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn color="red lighten-2" dark v-bind="attrs" v-on="on">
+            Click Me
+          </v-btn>
+        </template>
+
+        <v-card>
+          <v-card-title class="text-h5 grey lighten-2">
+            Privacy Policy
+          </v-card-title>
+
+          <v-card-text>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </v-card-text>
+
+          <v-divider></v-divider>
+
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn color="primary" text @click="buy_premium_dialog = false">
+              I accept
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog> -->
+    <!-- </div> -->
+  </v-card>
 </template>
 
 <script>
 export default {
-    data() {
-        return {
-          search:"",
-            headers: [{
-                    text: "Play",
-                    value: "play"
-                },
-                {
-                    text: "Title",
-                    value: "title"
-                },
-                {
-                    text: "Artist",
-                    value: "artist"
-                },
-                {
-                    text: "Album",
-                    value: "album"
-                },
-                {
-                    text: "Edit",
-                    value: "actions"
-                },
-            ],
-            loadingVar: true,
-            songs: [],
-        };
-    },
-    mounted() {
-        this.getSongs();
-    },
-    methods: {
-        getSongs: async function () {
-            let response = await this.$axios.$get("/songs");
-            this.songs = response;
-            this.loadingVar = false;
+  data() {
+    return {
+      buy_premium_dialog: false,
+      search: "",
+      headers: [
+        {
+          text: "Play",
+          value: "play",
         },
+        {
+          text: "Title",
+          value: "title",
+        },
+        {
+          text: "Artist",
+          value: "artist",
+        },
+        {
+          text: "Album",
+          value: "album",
+        },
+        {
+          text: "Edit",
+          value: "actions",
+          visible: false,
+        },
+      ],
+      loadingVar: true,
+      songs: [],
+    };
+  },
+  mounted() {
+    this.getSongs();
+  },
+  computed:{
+    filteredHeaders(){
+        if( this.$auth.user?.role!="premium"){
+         this.headers= this.headers.filter((item) => item.value != "actions");
+        }
+        return this.headers
+  },
+  },
+  methods: {
+    getSongs: async function () {
+      let response = await this.$axios.$get("/songs");
+      this.songs = response;
+      this.loadingVar = false;
+    },
 
-        // songClick(row) {
-        //   this.songs.map((item, index) => {
-        //     item.selected = item === row;
-        //     this.$set(this.songs, index, item);
-        //   });
-        //   this.$router.push("player/" + row.id);
-        // },
-        playSong(song) {
-            console.log(song);
-            this.$router.push("player/" + song.id);
-        },
-        editSong(song) {
-            console.log(song);
-            this.$router.push("edit_song/" + song.id);
-        },
+    // songClick(row) {
+    //   this.songs.map((item, index) => {
+    //     item.selected = item === row;
+    //     this.$set(this.songs, index, item);
+    //   });
+    //   this.$router.push("player/" + row.id);
+    // },
+    playSong(song) {
+      console.log(song);
+      this.$router.push("player/" + song.id);
     },
+    editSong(song) {
+    //   this.buy_premium_dialog = true;
+      console.log(song);
+      this.$router.push("edit_song/" + song.id);
+    },
+  },
 };
 </script>
